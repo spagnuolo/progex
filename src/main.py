37115455ad1f7dac4d64704c412d955810a2308a
devBarbar <barbarahmad@hotmail.de>
2020-06-23 17:18:41 +0200

    WIP

--- src/main.py
+++ src/main.py
@@ -20,12 +20,14 @@ def profile():
 
 
 @main.route('/newProduct')
+@login_required
 def new_product(barcode=''):
     categories =  db.get_all_product_categories()
     return render_template('newProduct.html', categories=categories, barcode=barcode)
 
 
 @main.route('/newProductEntry', methods=['POST'])
+@login_required
 def new_product_entry():
     product_id = db.new_product(request.form['name'], db.get_product_category_id_byname(request.form['group']), request.form['discription'])
     db.link_code_product(request.form['barcode'], product_id)
@@ -33,6 +35,7 @@ def new_product_entry():
 
 
 @main.route('/newItem', methods=['POST'])
+@login_required
 def new_item():
     if request.method == 'POST':
         code = request.form['barcode']
@@ -48,17 +51,20 @@ def new_item():
 
 
 @main.route('/newItemEntry', methods=['POST'])
+@login_required
 def new_item_entry():
     db.new_item(request.form['hid'], request.form['pid'], request.form['date'])
     return profile()
 
 
 @main.route('/scanner')
+@login_required
 def scanner():
     return render_template('scanner.html')
 
 
 @main.route('/scan', methods=['POST'])
+@login_required
 def scan():
     if request.method == 'POST':
         image, scancode = camera.barcode_locater(request.form['photo'])
@@ -66,6 +72,7 @@ def scan():
     return make_response(render_template('scanner.html', pic=image, barcode=scancode))
 
 @main.route('/dbraw')
+@login_required
 def dbraw():
     db.delete_scancode(42141112)
     return db.all_tables()
---
+++ src/static/js/scripts.js
@@ -0,0 +1,76 @@
+const swup = new Swup();
+
+const init = () => {
+  if (document.getElementById("scanner")) {
+    var width = 480;
+    var height = 0;
+    var streaming = false;
+    var video = null;
+    var canvas = null;
+    // var photo = null;
+    var button = null;
+
+    function loadCamera() {
+      video = document.getElementById("video");
+      canvas = document.getElementById("canvas");
+      // photo = document.getElementById("photo");
+      formPhoto = document.getElementById("formPhoto");
+      submitDiv = document.getElementById("submitDiv");
+      button = document.getElementById("button");
+
+      navigator.mediaDevices
+        .getUserMedia({ video: true, audio: false })
+        .then(function (stream) {
+          video.srcObject = stream;
+          video.play();
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
+
+      video.addEventListener(
+        "canplay",
+        function (ev) {
+          if (!streaming) {
+            height = video.videoHeight / (video.videoWidth / width);
+
+            if (isNaN(height)) {
+              height = width / (4 / 3);
+            }
+
+            video.setAttribute("width", width);
+            video.setAttribute("height", height);
+            canvas.setAttribute("width", width);
+            canvas.setAttribute("height", height);
+            streaming = true;
+          }
+        },
+        false
+      );
+
+      button.addEventListener(
+        "click",
+        function (ev) {
+          takepicture();
+        },
+        false
+      );
+    }
+
+    function takepicture() {
+      var context = canvas.getContext("2d");
+      canvas.width = width;
+      canvas.height = height;
+      context.drawImage(video, 0, 0, width, height);
+
+      var data = canvas.toDataURL("image/png");
+      /* photo.setAttribute("src", data); */
+      formPhoto.setAttribute("value", data);
+    }
+
+    loadCamera();
+  }
+};
+
+init();
+swup.on("contentReplaced", init);
